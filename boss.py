0957f99b70f7cb7e7508dc6a8dcddd2d60d5520d
nubskr <dakshrinwan2003@gmail.com>
2024-05-20 20:27:36 +0530

    formatting data to json

--- boss.py
+++ boss.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import sys
 
 url = 'https://codeforces.com/contest/1826/problem/A'
 
@@ -14,15 +16,12 @@ problem_statements = soup.find_all(class_='problem-statement')
 for statement in problem_statements:
     # title
     title = statement.find(class_='title').text.strip()
-    print("Title:", title)
     
     # time limit
     time_limit = statement.find(class_='time-limit').text.strip().replace('time limit per test', '')
-    print("Time Limit:", time_limit)
     
     # memory limit
     memory_limit = statement.find(class_='memory-limit').text.strip().replace('memory limit per test', '')
-    print("Memory Limit:", memory_limit)
     
     # input format
     input_spec = statement.find(class_='input-specification').text.strip().replace('Input', '')
@@ -35,23 +34,41 @@ for statement in problem_statements:
     # sample inputs
     examples = statement.find(class_='sample-tests')
     input_tests = examples.find_all(class_='test-example-line')
-    print("Input")
+
+    inputs = ""
     for i in input_tests: 
         input_example_lines = i.text.strip()
-        print(input_example_lines)
+        inputs += input_example_lines
+        inputs += '\n'
     
     # sample outputs
     output_tests = examples.find_all(class_='output')    
+    outputs = ""
+
+    # print(output_tests.text.strip())
     for i in output_tests:  
         output_example_lines = i.text.strip()
-        print(output_example_lines)
+        outputs += output_example_lines
+        outputs += '\n'
     
-    # extra shit
     note = statement.find(class_='note')
-    print(note)
+    notes = ""
+
     if note:
         note_text = note.text.strip().replace('Note', '')
-        print("Note:", note_text)
+        notes += note_text
+        # print("Note:", note_text)
     
-    print("\n")
+    data = {
+        "title": title,
+        "time limit": time_limit,
+        "memory limit": memory_limit,
+        "input format": input_spec,
+        "output format": output_spec,
+        "sample input": inputs,
+        "sample outputs": outputs,
+        "note": note_text
+    }
 
+    json_data = json.dumps(data, indent=4)
+    print(json_data)
